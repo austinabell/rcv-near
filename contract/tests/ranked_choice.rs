60cabbe240dcc69558a5b28d1c985afec66080f4
Phuong Nguyen <phuong@near.org>
2022-02-12 00:47:07 +0000

    Add simple test for working with workspaces

---
+++ contract/tests/ranked_choice.rs
@@ -0,0 +1,52 @@
+use workspaces::prelude::*;
+
+#[tokio::test]
+async fn test_ranked_choice_winner() -> anyhow::Result<()> {
+    let mut total_gas_burnt = 0;
+    let worker = workspaces::sandbox();
+    let alice = worker.dev_create_account().await?;
+    let contract = worker
+        .dev_deploy(include_bytes!("../../out/main.wasm"))
+        .await?;
+
+    contract
+        .call(&worker, "new")
+        .args_json(serde_json::json!({
+            "candidates": ["A".to_string(), "B".to_string(), "C".to_string()],
+        }))?
+        .transact()
+        .await?;
+    println!("Initialized contract");
+
+    let call_result = alice
+        .call(&worker, contract.id(), "vote")
+        .args_json(serde_json::json!({
+            "order": ["C".to_string(), "B".to_string(), "A".to_string()],
+        }))?
+        .transact()
+        .await?;
+    total_gas_burnt += call_result.total_gas_burnt;
+
+    let bob = worker.dev_create_account().await?;
+    let call_result = bob
+        .call(&worker, contract.id(), "vote")
+        .args_json(serde_json::json!({
+            "order": ["C".to_string(), "A".to_string(), "B".to_string()],
+        }))?
+        .transact()
+        .await?;
+    total_gas_burnt += call_result.total_gas_burnt;
+
+    println!("gas burnt: {} yoctoNear", total_gas_burnt);
+
+    let winner = contract
+        .call(&worker, "get_winner")
+        .view()
+        .await?
+        .json::<String>()?;
+
+    println!("OUR winner is: {}", winner);
+    assert_eq!(winner, "C");
+
+    Ok(())
+}
